04c500301fa93bbf9ef4fc5eac1cd0577a9d2685
Xiaolong Jiang <xiaolongx.jiang@intel.com>
2020-09-23 09:29:24 +0800

    modify vcl patch and remove ldp-lock patch
    Signed-off-by: Xiaolong Jiang <xiaolongx.jiang@intel.com>
    Change-Id: I4fe5f4dc2121d952bd0a4394afe0b18ce6f93516

--- packages/verify.sh
+++ packages/verify.sh
@@ -3,19 +3,14 @@
 ngxvcl=${BR}/_build/nginx_vcl
 ngxldp=${BR}/_build/nginx_ldp
 
+tls_tcp=tls
+
 function test_vcl(){
     echo ""
     echo "===================================================================="
     echo "Testing ..."
     echo ""
     export LD_LIBRARY_PATH=/usr/local/ssl/lib
-    cp ${BR}/configs/startup.conf ${BR}/configs/startup-test.conf
-
-    sed -i 's|/var/log/vpp/vpp.log|/tmp/vpp.log|' ${BR}/configs/startup-test.conf
-    sed -i 's|cli-vpp1.sock|cli.sock|' ${BR}/configs/startup-test.conf
-    sed -i 's|/path/to/configs/vppenvset|configs/vppset|' ${BR}/configs/startup-test.conf
-    sed -i 's|socket-name /path/to/vpp-api.sock|default|' ${BR}/configs/startup-test.conf
-    echo "set int state local0 up" > ${BR}/configs/vppset
 
     ${BR}/vpp/build-root/install-vpp-native/vpp/bin/vpp -c configs/startup-test.conf &
     vpp_pid=$!
@@ -31,9 +26,6 @@ function test_vcl(){
     export LD_LIBRARY_PATH=${BR}/vpp/build-root/install-vpp-native/vpp/lib
 
     sudo killall -v -s 9 nginx || echo "continue execute"
-
-    cp ${BR}/configs/nginx.conf ${BR}/configs/nginx-test.conf
-    sed -i 's|#worker_processes 1|worker_processes 1|' ${BR}/configs/nginx-test.conf
 
     ${ngxvcl}/objs/nginx -c ${BR}/configs/nginx-test.conf &
     nginx_pid=$!
@@ -60,10 +52,6 @@ function test_vcl(){
 
     sudo killall -v -s 9 nginx || echo ""
     sudo kill -9 ${vpp_pid} || echo ""
-    rm -f ${BR}/configs/nginx-test.conf
-    rm -f ${BR}/configs/startup-test.conf
-    rm -f ${BR}/configs/vppset
-    rm -f /tmp/vpp.log
 
     if [ ${ret} -eq 1 ]; then
         exit 1;
@@ -77,21 +65,11 @@ function test_ldp(){
     echo ""
 
     export LD_LIBRARY_PATH=/usr/local/ssl/lib
-    cp ${BR}/configs/startup.conf ${BR}/configs/startup-test.conf
-
-    sed -i 's|/var/log/vpp/vpp.log|/tmp/vpp.log|' ${BR}/configs/startup-test.conf
-    sed -i 's|cli-vpp1.sock|cli.sock|' ${BR}/configs/startup-test.conf
-    sed -i 's|/path/to/configs/vppenvset|configs/vppset|' ${BR}/configs/startup-test.conf
-    sed -i 's|socket-name /path/to/vpp-api.sock|default|' ${BR}/configs/startup-test.conf
-    echo "set int state local0 up" > ${BR}/configs/vppset
 
     ${BR}/vpp/build-root/install-vpp-native/vpp/bin/vpp -c configs/startup-test.conf &
     vpp_pid=$!
 
     sleep 5
-
-    cp ${BR}/configs/nginx.conf ${BR}/configs/nginx-test.conf
-    sed -i 's|#worker_processes 1|worker_processes 1|' ${BR}/configs/nginx-test.conf
 
     export VCL_CONFIG=${BR}/configs/vcl.conf
     export LDP_TRANSPARENT_TLS=1
@@ -113,7 +91,6 @@ function test_ldp(){
         ret=1
     fi
 
-
     v=`ps -A|grep "${nginx_pid}" | wc -l`
     if [ ${v} -eq 1 ]; then
         echo "LDP test: nginx                                               OK"
@@ -125,11 +102,6 @@ function test_ldp(){
 
     sudo killall -v -s 9 nginx || echo ""
     sudo kill -9 ${vpp_pid} || echo ""
-
-    rm -f ${BR}/configs/nginx-test.conf
-    rm -f ${BR}/configs/startup-test.conf
-    rm -f ${BR}/configs/vppset
-    rm -rf /tmp/vpp.log
 
     if [ ${ret} -eq 1 ]; then
         exit 1
